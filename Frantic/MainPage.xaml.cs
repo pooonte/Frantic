d49daf3e677421505db517b17a29d5406cf561da
ponte <maximmd@outlook.com>
2025-12-15 10:02:09 +0700

    .

--- Frantic/MainPage.xaml.cs
+++ Frantic/MainPage.xaml.cs
@@ -1,30 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
-
 namespace Frantic
 {
-    /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+            ContentFrame.Navigate(typeof(home));
+        }
+
+        private void NavButton_Click(object sender, RoutedEventArgs e)
+        {
+            string tag = (sender as Button)?.Tag?.ToString();
+            switch (tag)
+            {
+                case "tracks":
+                    ContentFrame.Navigate(typeof(tracks));
+                    break;
+                case "video":
+                    ContentFrame.Navigate(typeof(video));
+                    break;
+                case "photo":
+                    ContentFrame.Navigate(typeof(photo));
+                    break;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyPane.IsPaneOpen = !MyPane.IsPaneOpen;
         }
     }
 }
---
+++ Frantic/tracks.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Frantic
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class tracks : Page
+    {
+        public tracks()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
